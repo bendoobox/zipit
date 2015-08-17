861a820bb91c7aa9d97bdbdacaf8590e9bd0017a
Sander van de Graaf <mail@svdgraaf.nl>
2015-08-17 14:42:00 +0200

    Also tar the boot dir

--- zip-it.sh
+++ zip-it.sh
@@ -9,7 +9,10 @@ wget -O ./root/usr/bin/raspi-config https://raw.githubusercontent.com/bendoobox/
 cp -v ./root/usr/bin/raspi-config /usr/bin/raspi-config
 
 # tar all disk contents to tar file
-sudo tar -cvpf bendoobox.tar /* --exclude=proc/* --exclude=sys/* --exclude=dev/pts/* --exclude=var/cache/* --exclude=var/swap --exclude=home/pi/.bootstrap/*
+sudo tar -cvpf root.tar /* --exclude=proc/* --exclude=sys/* --exclude=dev/pts/* --exclude=var/cache/* --exclude=var/swap --exclude=home/pi/.bootstrap/*
+
+# tar all boot contents to tar file
+sudo tar -cvpf boot.tar /boot/*
 
 # xzip it
-echo "Now scp the file and xzip the file: xz -1 -k -e -v bendoobox.tar"
+echo "Now scp the file and xzip the file: xz -1 -k -e -v root.tar;xz -1 -k -e -v boot.tar"
